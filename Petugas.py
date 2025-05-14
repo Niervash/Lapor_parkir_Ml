8d2ff0e857a8a1534aafa9b6065d9a6e544d9e93
Julian Witjaksono <151512615+Niervash@users.noreply.github.com>
2025-05-14 19:13:37 +0800

    Update Petugas.py

--- Petugas.py
+++ Petugas.py
@@ -1,34 +1,37 @@
 import joblib as jb
 import pandas as pd
-from sklearn.metrics import confusion_matrix as sk_confusion_matrix, classification_report as sk_classification_report
+from sklearn.metrics import confusion_matrix as sk_confusion_matrix, classification_report as sk_classification_report, accuracy_score
 
 def read_model(filename):
     Model = jb.load(filename)
     print(f"Model loaded from {filename}")
     return Model
 
-def confusion_matrix(model, X_test, y_test):
+def confusion_matrix(model, X_test, y_test, return_accuracy=False):
     y_pred = model.predict(X_test)
     cm = sk_confusion_matrix(y_test, y_pred)
-    return cm
 
+    if return_accuracy:
+        accuracy = cm.diagonal().sum() / cm.sum()
+        return cm, accuracy
+
+    return cm
 
 def classification_report(model, X_test, y_test):
     y_pred = model.predict(X_test)
     report = sk_classification_report(y_test, y_pred, output_dict=True)
     return report
 
-def result(FILENAME, Lokasi, Identitas_Petugas):
+def result(FILENAME, Lokasi, Identitas_Petugas, y_true=None):
     try:
         # Load the model
         Model_loaded = read_model(FILENAME)
 
-
         errors = []
         if not Lokasi:
-            errors.append("Jenis_Kendaraan is empty.")
+            errors.append("Lokasi is empty.")
         if not Identitas_Petugas:
-            errors.append("Waktu is empty.")
+            errors.append("Identitas_Petugas is empty.")
 
         if errors:
             print("Error:", " ".join(errors))
@@ -39,15 +42,24 @@ def result(FILENAME, Lokasi, Identitas_Petugas):
             'Lokasi': Lokasi if isinstance(Lokasi, list) else [Lokasi],
             'Identitas Petugas': Identitas_Petugas if isinstance(Identitas_Petugas, list) else [Identitas_Petugas]
         })
+
         # Make predictions
         y_predictions = Model_loaded.predict(NewData)
 
+        # Calculate accuracy if y_true is provided
+        if y_true is not None:
+            accuracy = accuracy_score(y_true, y_predictions)
+            NewData['Akurasi Prediksi'] = [accuracy] * len(y_predictions)
+        else:
+            NewData['Akurasi Prediksi'] = ['N/A'] * len(y_predictions)
+
         # Append predicted statuses to DataFrame
         NewData['Status Pelaporan'] = y_predictions
-        print(NewData[['Lokasi', 'Identitas Petugas', 'Status Pelaporan']])
+        print(NewData[['Lokasi', 'Identitas Petugas', 'Akurasi Prediksi', 'Status Pelaporan']])
 
         # Return the necessary values
         return NewData['Lokasi'].tolist(), NewData['Identitas Petugas'].tolist(), NewData['Status Pelaporan'].tolist()
+
     except Exception as e:
         print(f"Error in result function: {str(e)}")
         return [], [], []
